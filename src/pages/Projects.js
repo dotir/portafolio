d48ce2fa91d22e0e951c8773e7284b581e4f0288
irving <irvingllerena@gmail.com>
2025-03-01 17:32:46 -0500

    grid col 2

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -38,7 +38,7 @@ const Projects = () => (
                 Cada uno representa diferentes desafíos y soluciones tecnológicas.
             </p>
             
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
                 {projects.map((project, index) => (
                     <ProjectCard key={index} project={project} />
                 ))}
